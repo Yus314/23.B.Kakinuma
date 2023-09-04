ac25711a5834832f9dd2c30fe4750eb4ae93b5d7
Yus314 <s20c3033tk@s.chibakoudai.jp>
2023-09-04 11:05:59 +0000

    model download

--- src/main.py
+++ src/main.py
@@ -16,20 +16,19 @@ from base64 import b64encode
 # Set device
 torch_device = "cuda" if torch.cuda.is_available() else "cpu"
 
+
 # 潜在空間を画像空間にデコードするためのVAEモデルを読み込む
-vae = AutoencoderKL.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="vae", token="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")
+vae = AutoencoderKL.from_pretrained('vae')
 
 # トークナイズとテキストのエンコード用に、tokenizerと、text_encoderを読み込む
-tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
-text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14")
+tokenizer=CLIPTokenizer.from_pretrained('tokenizer')
+text_encoder=CLIPTextModel.from_pretrained('text_encoder')
 
 # 潜在空間を生成するためのU-Netモデルの指定
-unet = UNet2DConditionModel.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="unet", token="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")
+unet = UNet2DConditionModel.from_pretrained('unet')
 
 # ノイズスケジューラの指定
-#scheduler = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000)
-scheduler =  DDIMScheduler.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="scheduler")
-
+scheduler = DDIMScheduler.from_pretrained('scheduler')
 # モデルをGPUへ移す
 vae = vae.to(torch_device)
 text_encoder = text_encoder.to(torch_device)
